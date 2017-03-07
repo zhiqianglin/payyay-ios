382e9ef8463336a0fd90e88a0ac8c49ead531a83
zhiqianglin <limcc321@gmail.com>
2017-03-06 21:36:57 -0500

    Initial Commit

---
+++ PayYay/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PayYay
+//
+//  Created by Zhiqiang Lin on 3/6/17.
+//  Copyright © 2017 Zhiqiang Lin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
